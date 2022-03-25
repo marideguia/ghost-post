507547bccf3da59fe4e0f0dbabac41a9af9bf6f2
Summer Warrican <summerwarrican7@hotmail.com>
2022-03-24 20:00:43 -0500

    Random Code added for Testing and fixing bugs

---
+++ ghost-post-app/src/components/RandomCode.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function RandomCode() {
+    
+ return(
+<div>
+<h1>Login Page</h1>
+<Link to="Car">Create Session</Link>
+</div>
+ );
+}
+export default RandomCode
