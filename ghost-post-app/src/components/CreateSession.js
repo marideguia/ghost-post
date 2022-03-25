7f5a83f89fad8458d06961087940c0d2f2789696
Summer Warrican <summerwarrican7@hotmail.com>
2022-03-24 21:57:22 -0500

    Updated Create Session

--- ghost-post-app/src/components/CreateSession.js
+++ ghost-post-app/src/components/CreateSession.js
@@ -4,8 +4,9 @@ import RandomCode from './RandomCode';
 import Posts from './Posts';
 import {BrowserRouter as Router, Routes, Route,Link } from 'react-router-dom';
 import 'tachyons';
+import './CreateSession.css'
 
-function Car() {
+function CreateSession() {
   const [text, setText] = useState("");
   const [text2, setText2] = useState("");
   const [text3, setText3] = useState("");
@@ -30,17 +31,21 @@ function Car() {
   return (
     //bg-light-blue br3 pa3 ma2 grow bw2 shadow-5 f4 fw6 db black link hover-dark-blue
     //bg-light-blue dib br3 pa3 ma2 bw2 shadow-5 dtc v-mid tc black ph3 ph4-l
-    <>
-     <div className = "bg-light-blue br3 pa3 ma2 bw2 shadow-5 f4 fw6 db black link hover-dark-blue dtc v-mid tc black ph3 ph4-l">
+    //bg-light-blue br3 pa3 ma2 bw2 shadow-5 f4 fw6 db black link  dtc v-mid tc black ph3 ph4-l
+    //bw2 shadow-5 f4 fw6 db
+    //"bg-light-blue dib br3 pa6"
+     <div className = "center vh-50 dt w-50 bg-light-blue br3 mt6" >
+       <div class="dtc v-mid tc black ph3 ph4-l">
+    <h1>Ghost Post Session Generator</h1>
     <h3>Enter title of your session below</h3>
     
     <div>
     <form onSubmit={handleSubmit}>
-        <input type="text" />
-        <button type="submit">Submit</button>
+        <input className='pa2 ba b--dark-gray ma2' type="text" />
+        <button className='f6 grow no-underline br-pill ba bw1 ph3 pv2 mb2 dib black' type="submit">Submit</button>
       </form>
-      <h1>{text}</h1>
-      <h2 className='shadow-5'>{text2}</h2>
+      <h2>{text}</h2>
+      <h2 className='dark-blue'>{text2}</h2>
       <h3>{text3}</h3>
       
     </div>
@@ -52,10 +57,11 @@ function Car() {
     show?null:<h4 className = "grow"><Link to="/">Go back to login page</Link></h4>
     }
     </div>
+    </div>
    </div> 
-  </>
+  
  
   );
 }
 
-  export default Car;
+  export default CreateSession;
