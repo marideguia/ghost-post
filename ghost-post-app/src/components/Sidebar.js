4f2ab96ed0a822d775c2de8de28fb5c9f8cb58cd
JishGrainofSalt <64856888+JishGrainofSalt@users.noreply.github.com>
2022-03-29 18:44:41 -0500

    Update Sidebar.js

--- ghost-post-app/src/components/Sidebar.js
+++ ghost-post-app/src/components/Sidebar.js
@@ -1,29 +1,45 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../sidebar.css";
 import {SidebarData} from './SidebarData';
 import { Link } from 'react-router-dom';
+import MenuIcon from '@material-ui/icons/Menu';
+import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import { IconContext } from 'react-icons'
+
 
 function SideBar() {
+  const [sidebar, setSidebar] = useState(false)
+  const showSidebar = () => setSidebar(!sidebar)
   return (
-    
+    <>
+    <IconContext.Provider value={{color:'gray'}}>
     <div className='SideBar'>
       
-      <ul className='SidebarList'>{SidebarData.map((val, key) => {
-      return (
-        
-        <li key={key} className='row' id={window.location.pathname === val.link ? "active" : ""}onClick={()=> {window.location.pathname = val.link}}> 
-          {" "}
-          <div id="icon">{val.icon}</div>{" "}
-          <div id='title'>
-          
-            {val.title}
-          </div>
-        </li>
-      );
-      })}
-      </ul>
+      <Link to="#" className='menu-bars'>
+        <MenuIcon onClick={showSidebar} />
+      </Link>
     </div>
-
+    <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+      <ul className='nav-menu-items' onClick={showSidebar}>
+        <li className='navbar-toggle'>
+          <Link to="#" className='menu-bars'>
+            <ArrowBackIosIcon/>
+          </Link>
+        </li>
+        {SidebarData.map((item, index) => {
+          return (
+            <li key = {index} className={item.clssname}>
+              <Link to={item.link}>
+                {item.icon}
+                <span>{item.title}</span>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+    </nav>
+    </IconContext.Provider>
+    </>
   )
 }
 
