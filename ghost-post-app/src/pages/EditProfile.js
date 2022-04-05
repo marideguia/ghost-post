d17c16883d58382356e05e239d75753d3950b6a8
JishGrainofSalt <64856888+JishGrainofSalt@users.noreply.github.com>
2022-04-05 18:43:14 -0500

    Update EditProfile.js

--- ghost-post-app/src/pages/EditProfile.js
+++ ghost-post-app/src/pages/EditProfile.js
@@ -1,69 +1,69 @@
-import React, { useState } from "react";
-import editUser from "../api/editUser";
+// import React, { useState } from "react";
+// import editUser from "../api/editUser";
 
-export default ({ changeToFalse, userDetails }) => {
-  const [name, setName] = useState("");
+// export default ({ changeToFalse, userDetails }) => {
+//   const [name, setName] = useState("");
 
-  const onSubmit = () => {
-    // const uid = firebaseApp.auth().currentUser.uid; Make mySQL version :)
+//   const onSubmit = () => {
+//     // const uid = firebaseApp.auth().currentUser.uid; put SQL version :)
 
-    if (image) {
-      console.log("Begin");
-      var uploadTask = storageRef.ref(`image/${image.name}`).put(image);
-      uploadTask.on(
-        "state_changed",
-        () => {},
-        error => {
-          console.log(error);
-        },
-        () => {
-          console.log("getting URL");
-          //Add URL to the Database
-          uploadTask.snapshot.ref.getDownloadURL().then(function(imageURL) {
-            const data = {
-              uid,
-              name,
-              email: userDetails.email
-            };
+//     if (image) {
+//       console.log("Begin");
+//       var uploadTask = storageRef.ref(`image/${image.name}`).put(image);
+//       uploadTask.on(
+//         "state_changed",
+//         () => {},
+//         error => {
+//           console.log(error);
+//         },
+//         () => {
+//           console.log("getting URL");
+//           //Add URL to the Database
+//           uploadTask.snapshot.ref.getDownloadURL().then(function(imageURL) {
+//             const data = {
+//               uid,
+//               name,
+//               email: userDetails.email
+//             };
 
-            const result = editUser(data);
+//             const result = editUser(data);
 
-            if (result === true) {
-              console.log("User Info Edited");
-            }
+//             if (result === true) {
+//               console.log("User Info Edited");
+//             }
 
-            if (result === false) {
-              console.log("ERROR");
-            }
-          });
-        }
-      );
-    } else {
-      const data = {
-        uid,
-        name
-      };
+//             if (result === false) {
+//               console.log("ERROR");
+//             }
+//           });
+//         }
+//       );
+//     } else {
+//       const data = {
+//         uid,
+//         name
+//       };
 
-      const result = editUser(data);
+//       const result = editUser(data);
 
-      if (result === true) {
-        console.log("User Info Edited");
-      }
+//       if (result === true) {
+//         console.log("User Info Edited");
+//       }
 
-      if (result === false) {
-        console.log("ERROR");
-      }
-    }
-  };
+//       if (result === false) {
+//         console.log("ERROR");
+//       }
+//     }
+//   };
 
-  return (
-    <div>
-      <div onClick={() => changeToFalse()}>Go Back</div>
-      <input
-        value={name}
-        onChange={event => setName(event.target.value)}
-      />
-      <button onClick={onSubmit}>Submit</button>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <div onClick={() => changeToFalse()}>Go Back</div>
+//       <input
+//         value={name}
+//         onChange={event => setName(event.target.value)}
+//       />
+//       <button onClick={onSubmit}>Submit</button>
+//     </div>
+//   );
+// };
