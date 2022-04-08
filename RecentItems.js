a133a097570acaf15276091e596189f2a0a76982
marideguia <82063754+marideguia@users.noreply.github.com>
2022-04-07 19:04:22 -0500

    Add files via upload

---
+++ RecentItems.js
@@ -0,0 +1,17 @@
+export const RecentItems= [
+    {
+        title: "Senior Capstone",
+        joinedAt: "",
+        students: 127,
+    },
+    {
+        title: "PHYS 2401",
+        joinedAt: "",
+        students: 117,
+    },
+    {
+        title: "Neural Networks",
+        joinedAt: "",
+        students: 92,
+    },
+]
