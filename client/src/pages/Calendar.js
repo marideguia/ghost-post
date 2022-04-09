42a56f1c97e2f4b8f9c0f195731bffcc99efdcf6
JishGrainofSalt <64856888+JishGrainofSalt@users.noreply.github.com>
2022-04-09 18:12:02 -0500

    Update Calendar.js

--- client/src/pages/Calendar.js
+++ client/src/pages/Calendar.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
+import SideBar from '../components/Sidebar';
+import Header from "../components/Header"
 
 
 function OurCalendar() {
   const [value, onChange] = useState(new Date());
 
   return (
-    <div>
-      <Calendar onChange={onChange} value={value} />
+    <div className='p-column-container'>
+        <Header title="Calendar"/>
+        <div className = "p-container">
+        <SideBar/>
+        </div>
+      <div>
+            <Calendar onChange={onChange} value={value} />
+      </div> 
     </div>
+    
   );
 }
 export default OurCalendar;
