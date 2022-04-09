2b7d277309b32ed10dcdc64334d3b190ab0761e3
JishGrainofSalt <64856888+JishGrainofSalt@users.noreply.github.com>
2022-04-09 16:23:02 -0500

    now it adapts to mobile

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -9,38 +9,65 @@ import { IconContext } from 'react-icons'
 
 function SideBar() {
   const [sidebar, setSidebar] = useState(false)
+  const nonMobileSidebar = useState(true)
   const showSidebar = () => setSidebar(!sidebar)
-  return (
-    <>
-    <IconContext.Provider value={{color:'gray'}}>
-    <div className='SideBar'>
-      
-      <Link to="#" className='menu-bars'>
-        <MenuIcon onClick={showSidebar} />
-      </Link>
-    </div>
-    <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
-      <ul className='nav-menu-items' onClick={showSidebar}>
-        <li className='navbar-toggle'>
-          <Link to="#" className='menu-bars'>
-            <ArrowBackIosIcon/>
-          </Link>
-        </li>
-        {SidebarData.map((item, index) => {
-          return (
-            <li key = {index} className={item.clssname}>
-              <Link to={item.link}>
-                {item.icon}
-                <span>{item.title}</span>
-              </Link>
-            </li>
-          )
-        })}
-      </ul>
-    </nav>
-    </IconContext.Provider>
-    </>
-  )
+  const keepSidebar = () => setSidebar(!nonMobileSidebar)
+  if (window.innerWidth <= 600){
+    return (
+      <>
+      <IconContext.Provider value={{color:'gray'}}>
+      <div className='SideBar'>
+        
+        <Link to="#" className='menu-bars'>
+          <MenuIcon onClick={showSidebar} />
+        </Link>
+      </div>
+      <nav className={sidebar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className='nav-menu-items' onClick={showSidebar}>
+          <li className='navbar-toggle'>
+            <Link to="#" className='menu-bars'>
+              <ArrowBackIosIcon/>
+            </Link>
+          </li>
+          {SidebarData.map((item, index) => {
+            return (
+              <li key = {index} className={item.clssname}>
+                <Link to={item.link}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+      </IconContext.Provider>
+      </>
+    )
+  }
+  else{
+    return (
+      <>
+      <IconContext.Provider value={{color:'gray'}}>
+      <nav className={nonMobileSidebar ? 'nav-menu active' : 'nav-menu'}>
+        <ul className='nav-menu-items' onClick={keepSidebar}>
+          {SidebarData.map((item, index) => {
+            return (
+              <li key = {index} className={item.clssname}>
+                <Link to={item.link}>
+                  {item.icon}
+                  <span>{item.title}</span>
+                </Link>
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+      </IconContext.Provider>
+      </>
+    )
+  }
+  
 }
 
 export default SideBar
