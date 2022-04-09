c64f69b4df5d4d7adece5a8d6a2f1011110e105c
JishGrainofSalt <64856888+JishGrainofSalt@users.noreply.github.com>
2022-04-09 17:00:55 -0500

    basic signup page
    will be edited for formatting later

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,9 +1,110 @@
-import React from 'react'
+import {React, useEffect,useState } from "react";
+import ReactDOM from "react-dom";
+import { Link } from 'react-router-dom';
+import "./Login.css";
 
-const SignUp = () => {
+
+function Signup() {
+  
+  const [errorMessages, setErrorMessages] = useState({});
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  
+
+  // User Login info
+  const database = [
+    {
+      username: "user1",
+      password: "pass1"
+    },
+    {
+      username: "user2",
+      password: "pass2"
+    }
+  ];
+
+  const errors = {
+    uname: "username is in use",
+  };
+
+
+
+  const handleSubmit = (event) => {
+    //Prevent page reload
+    event.preventDefault();
+
+    var { uname, pass } = document.forms[0];
+
+    // Find user login info
+    const userData = database.find((user) => user.username === uname.value);
+
+    // Compare user info
+    if (userData) {
+      // Username dupe found
+      setErrorMessages({ name: "uname", message: errors.uname });
+    }
+    else{
+      setIsSubmitted(true)
+    }
+  }
+
+  // Generate JSX code for error message
+  const renderErrorMessage = (name) =>
+    name === errorMessages.name && (
+      <div className="error">{errorMessages.message}</div>
+    );
+
+  // JSX code for login form
+  const renderForm = (
+      <div className="form">
+        <form onSubmit={handleSubmit}>
+          <div className="input-container">
+            <label className="login-label">Username </label>
+            <input type="text" name="uname" required />
+            {renderErrorMessage("uname")}
+          </div>
+
+          <div className="input-container">
+            <label className="login-label">Password</label>
+            <input type="text" name="pass" required />
+          </div>
+
+          <div className="btns-container">
+          
+          <div className="button-container">
+            <input className="lp-button" type="button" value = "Sign Up"/>
+          </div>
+          </div>
+          
+          
+        </form>
+        <div class="area" >
+            <ul class="circles">
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+            </ul>
+    </div >
+      </div>
+  );
+
   return (
-    <div>SignUp</div>
-  )
+    <div className="app">
+      <h1 className="signup-header1">Welcome to GhostPost</h1>
+      <div className="signup-form">
+      
+       
+        {isSubmitted ? <div>User is successfully signed up</div> : renderForm}
+      </div>
+    </div>
+  );
 }
 
-export default SignUp
+
+export default Signup;
