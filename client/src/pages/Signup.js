204b218e87d933a5d75053d2890d22b6d0a74360
muriegas23 <muriegas23@gmail.com>
2022-04-20 21:38:30 -0500

    Added Searchbar code and updated logo png and signup page

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,6 @@
-import {React, useEffect,useState } from "react";
+import React, {useEffect,useState } from "react";
 import ReactDOM from "react-dom";
 import { Link } from 'react-router-dom';
-import validator from "validator";
 import "./Login.css";
 
 
