82052ab963957a84235e07ac2e61eec0c6c326fa
JishGrainofSalt <64856888+JishGrainofSalt@users.noreply.github.com>
2022-04-29 20:16:53 -0500

    Add files via upload

---
+++ test-units/App.test.js
@@ -0,0 +1,14 @@
+import {render} from "@testing-library/react";
+import Sessions from "./components/Sessions";
+import App from "./App";
+import '@testing-library/jest-dom';
+import BrowserRouter from "react-router-dom";
+
+describe("renders login page", () => {
+    it("linked to right page", () =>{
+        const { container, getByText } = render(<App/>, ['/']);
+        const aboutLink = getByText('Welcome to GhostPost');
+        aboutLink.click();
+        expect(container).aboutLink;
+    });
+});
